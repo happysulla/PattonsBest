e4d7f53ae9c6b078efebaee94ac0c998d30f1b64
Chris <happysulla@gmail.com>
2026-03-15 13:39:32 -0400

    set default zoom to 1

--- PattonsBestGame/Properties/Settings.Designer.cs
+++ PattonsBestGame/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace PattonsBest.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.5")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public double ZoomCanvas {
             get {
                 return ((double)(this["ZoomCanvas"]));
